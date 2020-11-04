86345b6d002c1191fa765fab2648d5fa540957d6
Jonathan Schilling <jonathan.schilling@ipp.mpg.de>
2020-11-05 00:05:43 +0100

    removed unreferenced (serial) methods

--- Sources/General/fbal.f
+++ Sources/General/fbal.f
@@ -56,55 +56,6 @@
 
       END SUBROUTINE calc_fbal_par
 
-      SUBROUTINE calc_fbal(bsubu, bsubv)
-      USE vmec_main, ONLY: buco, bvco, equif, 
-     1                     jcurv, jcuru, chipf, vp, pres, 
-     2                     phipf, vpphi, presgrad, ohs
-#ifdef _ANIMEC
-     3                    ,pmap, pd, phot, tpotb, zero
-#endif
-      USE vmec_params, ONLY: signgs
-      USE vmec_dim, ONLY: ns, nrzt, nznt, ns1
-      USE realspace, ONLY: wint, phip
-#ifdef _ANIMEC
-     1                    ,pperp, ppar, onembc, sigma_an,
-     2                     pp1, pp2, pp3
-      USE vforces, gsqrt => azmn_o
-#endif
-
-      IMPLICIT NONE
-!-----------------------------------------------
-      REAL(dp), INTENT(in) :: bsubu(1:nrzt), bsubv(1:nrzt)
-!-----------------------------------------------
-!   L o c a l   V a r i a b l e s
-!-----------------------------------------------
-      INTEGER  :: js, lk
-      INTEGER :: nsmin, nsmax
-#ifdef _ANIMEC
-      REAL(dp) :: t4, t5
-#endif
-!-----------------------------------------------
-      DO js = 2, ns
-         buco(js) = SUM(bsubu(js:nrzt:ns)*wint(js:nrzt:ns))
-         bvco(js) = SUM(bsubv(js:nrzt:ns)*wint(js:nrzt:ns))
-      END DO
-
-!     FROM AMPERE'S LAW, JcurX are angle averages of jac*JsupX, so
-!                        JcurX = (dV/ds)/twopi**2 <JsupX> where <...> is flux surface average
-      DO js = 2, ns1
-         jcurv(js) = (signgs*ohs)*(buco(js+1) - buco(js))
-         jcuru(js) =-(signgs*ohs)*(bvco(js+1) - bvco(js))
-!FOR RFP vpphi(js) = (vp(js+1)/phip(js+1) + vp(js)/phip(js))/2
-         vpphi(js) = (vp(js+1) + vp(js))/2
-         presgrad(js) = (pres(js+1) - pres(js))*ohs
-         equif(js) = (-phipf(js)*jcuru(js) + chipf(js)*jcurv(js))
-     1                /vpphi(js) + presgrad(js)
-      END DO
-      equif(1) = 0
-      equif(ns) = 0
-
-      END SUBROUTINE calc_fbal
-
 #ifdef _ANIMEC
       SUBROUTINE bimax_ppargrad(pp1, pp2, pp3, ppar, onembc, pres, phot,
      1                          tpotb)
