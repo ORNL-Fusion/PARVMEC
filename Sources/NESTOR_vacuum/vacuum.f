2db15402506dbd482f4ae7e43457b3f4ab7d74fa
Jonathan Schilling <jonathan.schilling@mail.de>
2023-03-31 12:55:48 +0200

    fix typo: error message if dgetrs fails

--- Sources/NESTOR_vacuum/vacuum.f
+++ Sources/NESTOR_vacuum/vacuum.f
@@ -102,7 +102,7 @@ C-----------------------------------------------
       info = 0
       call dgetrs('No transpose', mnpd2, 1, amatrix, mnpd2, 
      &            ipiv, potvac, mnpd2, info)
-      IF (info .ne. 0) PRINT *, ' dgetrf error in scalpot'
+      IF (info .ne. 0) PRINT *, ' dgetrs error in scalpot'
       CALL second0(toff)
       timer_vac(tsolver) = timer_vac(tsolver) + (toff-ton)
       solver_time = solver_time + (toff - ton)
