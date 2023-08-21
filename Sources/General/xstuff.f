d0ff84f32041ae4d0385d2fd12a585b52efaaeba
Mark Cianciosa <cianciosamr@ornl.gov>
2023-08-21 15:39:01 -0400

    Adjust test tolarances.

--- Sources/General/xstuff.f
+++ Sources/General/xstuff.f
@@ -1,13 +1,13 @@
       MODULE xstuff
       USE stel_kinds, ONLY: rprec
+      USE vmec_state
+
       IMPLICIT NONE
 C-----------------------------------------------
 C   L o c a l   V a r i a b l e s
 C-----------------------------------------------
       REAL(rprec), DIMENSION(:), ALLOCATABLE ::
      1    gc, xcdot, xsave, xstore, scalxc, col_scale
-      REAL(rprec), DIMENSION(:), ALLOCATABLE, TARGET ::
-     1    xc
 
       REAL(rprec), DIMENSION(:), ALLOCATABLE ::
      1   pgc, pxcdot, pxsave, pxstore, pscalxc, pcol_scale
