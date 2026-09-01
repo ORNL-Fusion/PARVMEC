5e0a07f8e7ace02c280df68dc6f8b3e72e36dc72
Cianciosa <cianciosamr@ornl.gov>
2026-09-01 17:06:43 -0400

    Fix an issue where the inital multi_grid_equilibrium and multi_grid_v3post would have the wrong initial condition on the child processes. This is not an issue in normal reconstruction because they would used the default from the namelist as the inital parameter values. Fix by John Schmitt.

--- Sources/General/vmec_params.f
+++ Sources/General/vmec_params.f
@@ -10,26 +10,26 @@ C-----------------------------------------------
 
       INTEGER, PRIVATE :: ink
       INTEGER, PARAMETER, DIMENSION(0:mpold) ::
-     1  jmin1 = (/ 1,1,(2,ink=2,mpold) /),        !starting js(m) values where R,Z are non-zero
-     2  jmin2 = (/ 1,2,(2,ink=2,mpold) /),        !starting js(m) values for which R,Z are evolved
-     3  jlam  = (/ 2,2,(2,ink=2,mpold) /)         !starting js(m) values for which Lambda is evolved
+     &  jmin1 = (/ 1,1,(2,ink=2,mpold) /),        !starting js(m) values where R,Z are non-zero
+     &  jmin2 = (/ 1,2,(2,ink=2,mpold) /),        !starting js(m) values for which R,Z are evolved
+     &  jlam  = (/ 2,2,(2,ink=2,mpold) /)         !starting js(m) values for which Lambda is evolved
 
 !  Besure to update werror in fileout.f when adding more error flags.
       INTEGER, PARAMETER :: norm_term_flag=0, bad_jacobian_flag=1, 
-     1                      more_iter_flag=2, 
-     2                      jac75_flag=4, input_error_flag=5,
-     3                      phiedge_error_flag=7,
-     4                      ns_error_flag=8,
-     5                      misc_error_flag=9,
-     6                      successful_term_flag=11, !ftol force criterion has been met
-     7                      bsub_bad_js1_flag=12,
-     8                      r01_bad_value_flag=13,
-     9                      arz_bad_value_flag=14
+     &                      more_iter_flag=2,
+     &                      jac75_flag=4, input_error_flag=5,
+     &                      phiedge_error_flag=7,
+     &                      ns_error_flag=8,
+     &                      misc_error_flag=9,
+     &                      successful_term_flag=11, !ftol force criterion has been met
+     &                      bsub_bad_js1_flag=12,
+     &                      r01_bad_value_flag=13,
+     &                      arz_bad_value_flag=14
       INTEGER, PARAMETER :: restart_flag=1, readin_flag=2,
-     1                      timestep_flag=4,output_flag=8, 
-     2                      cleanup_flag=16, reset_jacdt_flag=32
-    
-      REAL(rprec), PARAMETER :: pdamp = 0.05_dp  
+     &                      timestep_flag=4,output_flag=8,
+     &                      cleanup_flag=16, reset_jacdt_flag=32
+
+      REAL(rprec), PARAMETER :: pdamp = 0.05_dp
       CHARACTER(LEN=*), PARAMETER :: version_ = '10.0'
 !-----------------------------------------------
 !   L o c a l   V a r i a b l e s
