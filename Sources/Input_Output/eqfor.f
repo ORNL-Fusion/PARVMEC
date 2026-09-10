db51f94d1be4e3135da7fa5a44dd55d257a0cc64
CharlesCNorton <135471798+CharlesCNorton@users.noreply.github.com>
2026-09-10 11:18:25 -0400

    Carry the Jacobian sign in chi behind USE_UNSIGNED_CHI

--- Sources/Input_Output/eqfor.f
+++ Sources/Input_Output/eqfor.f
@@ -180,7 +180,11 @@ C-----------------------------------------------
          chi1(i) = chi1(i - 1) + hs*(phip(i)*iotas(i))
       END DO
 
+#ifdef UNSIGNED_CHI
       chi = twopi*chi1
+#else
+      chi = twopi*signgs*chi1
+#endif
 
 !	WRITE (36, 201) (i, phi1(i), chi1(i), iotas(i), i=1, ns)
 !201   FORMAT (i4, 1p,3e14.6)
