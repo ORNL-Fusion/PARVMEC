0a287bf2612eae32e6b7c190123df34a72f9c6d2
CharlesCNorton <machineelv@gmail.com>
2026-09-25 16:55:03 -0400

    Bring a lasym boundary to RBS(m=1) = ZBC(m=1) whichever way theta runs (#93)
    * Bring a lasym boundary to RBS(m=1) = ZBC(m=1) whichever way theta runs
    
    * Move the boundary conversion of readin into reset_boundary
    
    * Document init_geometry, flip_theta and reset_boundary with doxygen comments
    
    * Raise the aspect tolerance of the orientation test to 1.4E-13

--- Sources/Initialization_Cleanup/init_geometry.f90
+++ Sources/Initialization_Cleanup/init_geometry.f90
@@ -1,9 +1,36 @@
+!*******************************************************************************
+!>  @file init_geometry.f90
+!>  @brief Contains module @ref init_geometry.
+!
+!  Note separating the Doxygen comment block here so detailed description is
+!  found in the Module not the file.
+!
+!>  Defines the routines that set the boundary of a run from its input
+!>  coefficients and orient the poloidal angle so that the Jacobian takes the
+!>  sign VMEC assumes.
+!*******************************************************************************
       MODULE INIT_GEOMETRY
 
+!>  True when @ref reset_boundary flipped theta -> pi - theta because the
+!>  boundary was given with the opposite orientation. profil1d negates iotas
+!>  and chips to match.
       LOGICAL     :: lflip
 
       CONTAINS
 
+!-------------------------------------------------------------------------------
+!>  @brief Flip the poloidal angle, theta -> pi - theta.
+!>
+!>  Multiplies the cos(m*theta) amplitudes by (-1)^m and the sin(m*theta)
+!>  amplitudes by (-1)^(m+1), which reverses the direction theta runs around
+!>  each surface and with it the sign of the Jacobian. The m = 0 amplitudes are
+!>  unchanged.
+!>
+!>  @param[inout] rmn Fourier amplitudes of R, indexed by n, m and parity.
+!>  @param[inout] zmn Fourier amplitudes of Z, indexed by n, m and parity.
+!>  @param[inout] lmn Fourier amplitudes of lambda, indexed by n, m and parity.
+!>                    Optional.
+!-------------------------------------------------------------------------------
       SUBROUTINE flip_theta(rmn, zmn, lmn)
       USE vmec_main
       USE vmec_params, ONLY: ntmax, rcc, rss, zsc, zcs,                 &
@@ -54,5 +81,187 @@
       END DO
 
       END SUBROUTINE flip_theta
+
+!-------------------------------------------------------------------------------
+!>  @brief Set the internal boundary of a run from its input coefficients.
+!>
+!>  Fills rmn_bdy and zmn_bdy from the input coefficients rbc, zbs, rbs and zbc,
+!>  sets lflip and signgs, and flips a boundary given with the opposite
+!>  orientation with @ref flip_theta. The orientation is the sign of the m = 1
+!>  determinant at zeta = 0,
+!>
+!>    SUM(rbc(:,1))*SUM(zbs(:,1)) - SUM(rbs(:,1))*SUM(zbc(:,1)),
+!>
+!>  whose second term is absent without lasym and which no rotation of theta
+!>  changes. For lasym the coefficients are first rotated in theta so that
+!>  RBS(m=1) = ZBC(m=1) holds after the flip. The rotation changes rbc, zbs, rbs
+!>  and zbc in place, and a second call finds a rotation angle of zero to
+!>  round-off. Called by readin, and again by any caller that changes the
+!>  boundary coefficients.
+!-------------------------------------------------------------------------------
+      SUBROUTINE reset_boundary
+      USE vmec_main
+      USE vmec_params, ONLY: rcc, rss, zsc, zcs, zcc, zss, rsc, rcs,    &
+                             signgs
+      IMPLICIT NONE
+!-----------------------------------------------
+!   L o c a l   V a r i a b l e s
+!-----------------------------------------------
+      INTEGER :: m, n, mj, ni, isgn, ioff, joff
+      REAL(rprec) :: delta, orient, trc, tzc
+      REAL(rprec), DIMENSION(:,:), POINTER ::                           &
+         rbcc, rbss, rbcs, rbsc, zbcs, zbsc, zbcc, zbss
+      REAL(rprec), ALLOCATABLE :: temp(:)
+!-----------------------------------------------
+!
+!     CONVERT TO REPRESENTATION WITH RBS(m=1) = ZBC(m=1). THE m=1
+!     DETERMINANT, WHICH NO ROTATION OF THETA CHANGES, IS NEGATIVE FOR A
+!     BOUNDARY THAT FLIP_THETA REVERSES BELOW; THAT ONE IS ROTATED SO
+!     THAT THE REPRESENTATION HOLDS AFTER THE FLIP
+!
+      IF (lasym) THEN
+         orient = SUM(rbc(-ntor:ntor,1))*SUM(zbs(-ntor:ntor,1))         &
+                - SUM(rbs(-ntor:ntor,1))*SUM(zbc(-ntor:ntor,1))
+         IF (orient .lt. zero) THEN
+            delta = -ATAN2(rbs(0,1) + zbc(0,1), zbs(0,1) - rbc(0,1))
+         ELSE IF (rbc(0,1) + zbs(0,1) .gt. zero) THEN
+            delta = ATAN((rbs(0,1) - zbc(0,1))/                         &
+                         (rbc(0,1) + zbs(0,1)))
+         ELSE
+            delta = ATAN2(rbs(0,1) - zbc(0,1), rbc(0,1) + zbs(0,1))
+         END IF
+         IF (delta .ne. zero) THEN
+            DO m = 0,mpol1
+               DO n = -ntor,ntor
+                  trc = rbc(n,m)*COS(m*delta) + rbs(n,m)*SIN(m*delta)
+                  rbs(n,m) = rbs(n,m)*COS(m*delta)                      &
+                           - rbc(n,m)*SIN(m*delta)
+                  rbc(n,m) = trc
+                  tzc = zbc(n,m)*COS(m*delta) + zbs(n,m)*SIN(m*delta)
+                  zbs(n,m) = zbs(n,m)*COS(m*delta)                      &
+                           - zbc(n,m)*SIN(m*delta)
+                  zbc(n,m) = tzc
+               END DO
+            END DO
+         END IF
+      END IF
+
+!
+!     CONVERT TO INTERNAL REPRESENTATION OF MODES
+!
+!     R = RBCC*COS(M*U)*COS(N*V) + RBSS*SIN(M*U)*SIN(N*V)
+!       + RBCS*COS(M*U)*SIN(N*V) + RBSC*SIN(M*U)*COS(N*V)
+!     Z = ZBCS*COS(M*U)*SIN(N*V) + ZBSC*SIN(M*U)*COS(N*V)
+!       + ZBCC*COS(M*U)*COS(N*V) + ZBSS*SIN(M*U)*SIN(N*V)
+!
+!
+!     POINTER ASSIGNMENTS (NOTE: INDICES START AT 1, NOT 0, FOR POINTERS, EVEN THOUGH
+!                          THEY START AT ZERO FOR RMN_BDY)
+!     ARRAY STACKING ORDER DETERMINED HERE
+!
+      rbcc => rmn_bdy(:,:,rcc)
+      zbsc => zmn_bdy(:,:,zsc)
+      IF (lthreed) THEN
+         rbss => rmn_bdy(:,:,rss)
+         zbcs => zmn_bdy(:,:,zcs)
+      END IF
+
+      IF (lasym) THEN
+         rbsc => rmn_bdy(:,:,rsc)
+         zbcc => zmn_bdy(:,:,zcc)
+         IF (lthreed) THEN
+            rbcs => rmn_bdy(:,:,rcs)
+            zbss => zmn_bdy(:,:,zss)
+         END IF
+      END IF
+
+      rmn_bdy = 0
+      zmn_bdy = 0
+
+      ioff = LBOUND(rbcc,1)
+      joff = LBOUND(rbcc,2)
+
+      DO m = 0, mpol1
+         mj = m + joff
+         IF (lfreeb .and.                                               &
+             (mfilter_fbdy.gt.1 .and. m.gt.mfilter_fbdy)) THEN
+            CYCLE
+         END IF
+         DO n = -ntor, ntor
+            IF (lfreeb .and.                                            &
+                (nfilter_fbdy.gt.0 .and. ABS(n).gt.nfilter_fbdy)) THEN
+               CYCLE
+            END IF
+            ni = ABS(n) + ioff
+            IF (n .eq. 0) THEN
+               isgn = 0
+            ELSE IF (n .gt. 0) THEN
+               isgn = 1
+            ELSE
+               isgn = -1
+            END IF
+            rbcc(ni,mj) = rbcc(ni,mj) + rbc(n,m)
+            IF (m .gt. 0) THEN
+               zbsc(ni,mj) = zbsc(ni,mj) + zbs(n,m)
+            END IF
+
+            IF (lthreed) THEN
+               IF (m .gt. 0) THEN
+                  rbss(ni,mj) = rbss(ni,mj) + isgn*rbc(n,m)
+               END IF
+               zbcs(ni,mj) = zbcs(ni,mj) - isgn*zbs(n,m)
+            END IF
+
+            IF (lasym) THEN
+               IF (m .gt. 0) THEN
+                  rbsc(ni,mj) = rbsc(ni,mj) + rbs(n,m)
+               END IF
+               zbcc(ni,mj) = zbcc(ni,mj) + zbc(n,m)
+               IF (lthreed) THEN
+                  rbcs(ni,mj) = rbcs(ni,mj) - isgn*rbs(n,m)
+                  IF (m .gt. 0) THEN
+                     zbss(ni,mj) = zbss(ni,mj) + isgn*zbc(n,m)
+                  END IF
+               END IF
+            END IF
+         END DO
+      END DO
+
+!
+!     CHECK SIGN OF JACOBIAN (SHOULD BE SAME AS SIGNGS)
+!     FOR lasym, ORIENT IS THE m=1 DETERMINANT TAKEN ABOVE
+!
+      mj = 1 + joff
+      IF (.not.lasym) THEN
+         orient = SUM(rbcc(1:ntor1,mj))*SUM(zbsc(1:ntor1,mj))
+      END IF
+      lflip = (orient .lt. zero)
+      signgs = -1
+      IF (lflip) THEN
+         CALL flip_theta(rmn_bdy, zmn_bdy)
+      END IF
+
+!
+!     CONVERT TO INTERNAL FORM FOR (CONSTRAINED) m=1 MODES
+!     INTERNALLY, FOR m=1: XC(rss) = .5(RSS+ZCS), XC(zcs) = .5(RSS-ZCS)
+!     WITH XC(zcs) -> 0 FOR POLAR CONSTRAINT
+!     (see convert_sym, convert_asym in totzsp_mod file)
+!
+      IF (lconm1 .and. (lthreed .or. lasym)) THEN
+         ALLOCATE (temp(SIZE(rbcc,1)))
+         IF (lthreed) THEN
+            temp = rbss(:,mj)
+            rbss(:,mj) = cp5*(temp(:) + zbcs(:,mj))
+            zbcs(:,mj) = cp5*(temp(:) - zbcs(:,mj))
+         END IF
+         IF (lasym) THEN
+            temp = rbsc(:,mj)
+            rbsc(:,mj) = cp5*(temp(:) + zbcc(:,mj))
+            zbcc(:,mj) = cp5*(temp(:) - zbcc(:,mj))
+         END IF
+         DEALLOCATE (temp)
+      END IF
+
+      END SUBROUTINE reset_boundary
 
       END MODULE INIT_GEOMETRY
